7b9af7f7af596a8f3b79ac35a77a140555593b84
Adnan <adnan.sayyed.1947@gmail.com>
2019-11-01 14:19:11 +0530

    added binary save system

---
+++ Assets/GameData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameData : MonoBehaviour
+{
+
+    public int GameInteger { get; set; }
+    public string GameString { get; set; }
+
+    [SerializeField]
+    private Text textInteger;
+
+    public void GenerateNewData()
+    {
+        GameInteger = Random.Range(1, 300);
+        //GameString = System.Convert.ToBase64String(System.BitConverter.GetBytes(inputString));
+        ShowData();
+    }
+
+    public void ShowData()
+    {
+        textInteger.text = GameInteger.ToString();
+    }
+    
+   
+}
---
+++ Assets/SaveScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using System.IO;
+
+public class SaveScript : MonoBehaviour
+{
+
+    private GameData gameData;
+    private string savePath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameData = GetComponent<GameData>();
+        savePath = Application.persistentDataPath + "/gamesave.save";
+    }
+    public void SaveData()
+    {
+        var save = new Save()
+        {
+            savedInteger = gameData.GameInteger
+        };
+
+        var binaryFormatter = new BinaryFormatter();
+        using(var fileStream = File.Create(savePath))
+        {
+            binaryFormatter.Serialize(fileStream, save);
+        }
+
+        print("Data Saved");
+    }
+
+
+    public void LoadData()
+    {
+        if (File.Exists(savePath))
+        {
+            Save save;
+            var binaryFormatter = new BinaryFormatter();
+            using(var fileStream = File.Open(savePath, FileMode.Open))
+            {
+                save = (Save)binaryFormatter.Deserialize(fileStream);
+
+            }
+
+            gameData.GameInteger = save.savedInteger;
+            gameData.ShowData();
+
+            print("Data Loaded");
+        }
+    }
+   
+}
